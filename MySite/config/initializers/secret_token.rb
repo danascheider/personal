0db1c76494909913b1102b56553f8acff897eb98
danascheider <dana.scheider@gmail.com>
2014-05-04 16:38:40 -0700

    Ran 'rails new MySite'

---
+++ MySite/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySite::Application.config.secret_key_base = '2a070f32049d1e4e8f30e4187ded3c0825f507bdf1e265e15e0d8926d260c21b3fda3f820b60bd827d3e71d4d65c5f665c32da480b2f6f7858b636b26c2448b9'
